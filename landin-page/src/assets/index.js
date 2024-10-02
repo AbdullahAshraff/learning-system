80108896941b26d86c6758c9b3848902b81ce72d
mai mahmoud <maiebrahim31.mm@gmail.com>
2024-10-02 15:57:34 +0300

    removed

--- landin-page/src/assets/index.js
+++
@@ -1,6 +0,0 @@
-import team from './team.png';
-import nlogo from './nlogo.png';
-import learning from './learning.jpg';
-import learn from './learn.png';
-import increase from './increase.png'
-export{team,nlogo,learning,learn,increase};
