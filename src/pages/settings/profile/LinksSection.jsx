521309679b543fb1be1d0fd41148b90d65ddeca0
AbdullahAshraff <125521810+AbdullahAshraff@users.noreply.github.com>
2024-10-02 21:18:55 +0300

    fix wrap links urls

--- src/pages/settings/profile/LinksSection.jsx
+++ src/pages/settings/profile/LinksSection.jsx
@@ -6,17 +6,17 @@ const LinksSection = ({ user }) => {
       <div className="grid grid-cols-1 md:grid-cols-[160px_1fr] max-w-[800px] gap-2 md:gap-5">
         <div className="field-link-name">Github</div>
         <div className="field-link-value">
-          <div className="break-words w-full">{user.links.github.url}</div>
-          <EditIconButton />{' '}
+          <div className="break-words max-w-full">{user.links.github.url}</div>
+          <EditIconButton />
         </div>
         <div className="field-link-name">LinkedIn</div>
         <div className="field-link-value">
-          <div className="break-words w-full">{user.links.linkedin.url}</div>
+          <div className="break-words max-w-full">{user.links.linkedin.url}</div>
           <EditIconButton />
         </div>
         <div className="field-link-name">Website</div>
         <div className="field-link-value">
-          <div>{user.links.website.url}</div>
+          <div className='break-words max-w-full'>{user.links.website.url}</div>
           <EditIconButton />
         </div>
       </div>
