ebf86c12c5c54d719cf571058725ddebe8798e2f
AbdullahAshraff <125521810+AbdullahAshraff@users.noreply.github.com>
2024-10-24 18:27:24 +0300

    lazy loading and loading for MainLayout

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,13 +1,23 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar.jsx';
 import Footer from '../components/Footer.jsx';
+import { Suspense } from 'react';
+import Loading from '../components/LoadingCircularProgress.jsx';
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <NavBar />
       <div className="flex-1">
-        <Outlet />
+        <Suspense
+          fallback={
+            <div className="h-screen">
+              <Loading />
+            </div>
+          }
+        >
+          <Outlet />
+        </Suspense>
       </div>
       <Footer />
     </div>
